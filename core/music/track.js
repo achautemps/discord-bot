a3265125eafae8e7b57af5f73389c6989b1b9319
Alex Chautemps <chautemps.alexandre@gmail.com>
2022-10-16 14:26:11 +0200

    feat(music) : add a queue and track logs

--- core/music/track.js
+++ core/music/track.js
@@ -1,18 +1,17 @@
-
-
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const ytdl = require("ytdl-core-discord");
-const {
-  createAudioResource,
-} = require("@discordjs/voice");
+const { createAudioResource } = require("@discordjs/voice");
 
 export default class Track {
-  constructor({ url }) {
+  constructor(url, { onStart, onFinish, onError }) {
     this.url = url;
+    this.onStart = onStart ? onStart : () => {};
+    this.onFinish = onFinish ? onFinish : () => {};
+    this.onError = onError ? onError : () => {};
   }
   async getAudioResource() {
-    const stream = await ytdl(this.url, { filter: 'audioonly' });
-    return createAudioResource(stream);
+    const stream = await ytdl(this.url, { filter: "audioonly" });
+    return createAudioResource(stream, { metadata: this });
   }
 }
